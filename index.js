c49cfbb0c5b70fbc4c1c80cc5cc3990e24eae2b2
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2025-03-17 16:53:54 +0200

    feat: add part3 phonebook(backend) step3,4 - 3.15,3,16

--- index.js
+++ index.js
@@ -6,12 +6,19 @@ const cors = require('cors')
 
 const app = express();
 
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' });
+    }
+
+    next(error); // Forward other errors
+  };
+
 app.use(cors())
-
 app.use(express.static('dist'))
-
 app.use(morgan('tiny'));
-
 app.use(express.json());
 
 // Create custom token to log request body
@@ -41,13 +48,18 @@ app.get('/api/persons', (request, response) => {
       })
 });
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(person => {
-      response.json(person)
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
     })
+    .catch(error => next(error))
   })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id)
       .then(result => {
         if (result) {
@@ -56,7 +68,7 @@ app.delete('/api/persons/:id', (request, response) => {
           response.status(404).json({ error: 'Note not found' });
         }
       })
-      .catch(error => response.status(400).json({ error: 'Invalid ID format' }));
+      .catch(error => next(error));
   });
 
 app.post('/api/persons', (request, response) => {
@@ -75,6 +87,32 @@ app.post('/api/persons', (request, response) => {
       response.json(savedPerson)
     })
   })
+
+  app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+      .then(updatedPerson => {
+        if (updatedPerson) {
+          response.json(updatedPerson)
+        } else {
+          response.status(404).end()
+        }
+      })
+      .catch(error => next(error))
+  })
+
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+
+  app.use(unknownEndpoint)
+  app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
