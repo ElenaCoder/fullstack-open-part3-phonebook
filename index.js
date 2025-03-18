b388c5f3dd2e9d7366c2929bb61f466d466ab68f
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2025-03-18 14:19:29 +0200

    feat: add part3 phonebook(backend) step7 - 3.19

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ const cors = require('cors')
 const app = express();
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message);
+    console.error('Error message:', error.message);
 
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' });
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message });
     }
 
     next(error); // Forward other errors
