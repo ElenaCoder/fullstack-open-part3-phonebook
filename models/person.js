3031d656941444334a34ac1f0404b0d3fc8d7942
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2025-03-18 14:52:17 +0200

    feat: add part3 phonebook(backend) step8 - 3.20

--- models/person.js
+++ models/person.js
@@ -22,7 +22,16 @@ const personSchema = new mongoose.Schema({
         minLength: 3,
         required: true
       },
-    number: String,
+    number: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function (v) {
+                return /^\d{2,3}-\d{5,}$/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number! A valid phone number must be in the format XX-XXXXX or XXX-XXXXX (at least 8 digits in total).`
+        }
+    }
 });
 
 personSchema.set('toJSON', {
