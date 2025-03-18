8a937b5cc44697da07a7891e4cfa339d7d2716c9
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2025-03-18 18:45:58 +0200

    refactor: apply formatter phonebook(backend) part3

--- models/person.js
+++ models/person.js
@@ -20,8 +20,8 @@ const personSchema = new mongoose.Schema({
     name: {
         type: String,
         minLength: 3,
-        required: true
-      },
+        required: true,
+    },
     number: {
         type: String,
         required: true,
@@ -29,9 +29,10 @@ const personSchema = new mongoose.Schema({
             validator: function (v) {
                 return /^\d{2,3}-\d{5,}$/.test(v);
             },
-            message: props => `${props.value} is not a valid phone number! A valid phone number must be in the format XX-XXXXX or XXX-XXXXX (at least 8 digits in total).`
-        }
-    }
+            message: (props) =>
+                `${props.value} is not a valid phone number! A valid phone number must be in the format XX-XXXXX or XXX-XXXXX (at least 8 digits in total).`,
+        },
+    },
 });
 
 personSchema.set('toJSON', {
